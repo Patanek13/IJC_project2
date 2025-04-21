1af7c9e901e3d4081cbcc3fa9d02574c4750e294
Patrik Lostak <patrik.lostak@gmail.com>
2025-04-21 23:31:15 +0200

    Changing parameter name

--- htab_bucket_count.c
+++ htab_bucket_count.c
@@ -1,4 +1,4 @@
-// htab_size.c
+// htab_bucket_count.c
 // Řešení IJC-DU2, příklad 2), 21.4.2025
 // Autor: Patrik Lošťák, FIT
 // Přeloženo: gcc 13.3.0
@@ -7,6 +7,6 @@
 #include "htab_t.h"
 
 // Returns the number of records in the hash table
-size_t htab_size (const htab_t *t) {
-    return t->size;
+size_t htab_bucket_count(const htab_t *t) {
+    return t->arr_size;
 }
