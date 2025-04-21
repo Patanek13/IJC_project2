0fca7eac1f7bb1450de0a1527c2b1d94f4af200f
Patrik Lostak <patrik.lostak@gmail.com>
2025-04-21 22:23:16 +0200

    Creating hash function for hash-table

---
+++ htab_hash_function.c
@@ -0,0 +1,17 @@
+// htab_hash_function.h
+// Řešení IJC-DU2, příklad 2), 21.4.2025
+// Autor: Patrik Lošťák, FIT
+// Přeloženo: gcc 13.3.0
+
+#include "htab.h"
+#include <stdint.h>    // uint32_t
+
+// Hash function for strings
+// This function computes a hash value for a given string using the 65599 hash algorithm.
+size_t htab_hash_function(const char *str) {
+    uint32_t h = 0;     // musí mít 32 bitů
+    const unsigned char *p;
+    for(p = (const unsigned char*)str; *p != '\0'; p++)
+        h = 65599 * h + *p;
+    return h;
+  }
